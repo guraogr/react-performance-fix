5b9fac34edb4ffe8cac25b519c621ee6bcf75bd4
guraogr <guraogr@gmail.com>
2022-06-27 00:38:37 +0900

    refactor: ChildComponentをメモ化

--- src/components/ChildComponent.tsx
+++ src/components/ChildComponent.tsx
@@ -1,14 +1,16 @@
+import { memo } from "react";
 interface Props {
   onClick: () => void;
 }
 
-const ChildComponent = ({ onClick }: Props) => {
+// メモ化させることで、propsに変更がない時はレンダリングを止めることができる。
+const ChildComponent = memo(({ onClick }: Props) => {
   console.log("子のコンポーネントが描画されました。");
   return (
     <button type="button" onClick={onClick}>
       +2
     </button>
   );
-};
+});
 
 export default ChildComponent;
